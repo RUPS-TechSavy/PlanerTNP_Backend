7a5ce05475fd929654bb40deb863b7021dbb91d5
Anja Ostovršnik <61201059+Anja159@users.noreply.github.com>
2025-01-03 16:54:26 +0100

    Notifications backend
    Notification 10 and 15 minutes before the task begins.

--- endpoints/task_logic.py
+++ endpoints/task_logic.py
@@ -27,12 +27,13 @@ def get_all_tasks(user_id):
         return {"error": "User not found"}, 404
 
     tasks = user.get("tasks", [])
+    schedule_task_reminders(tasks, user_id)
+
     # Convert task IDs to strings for JSON serialization
     for task in tasks:
         task["_id"] = str(task["_id"])
 
     return {"tasks": tasks}, 200
-
 
 # user_logic.py
 
@@ -48,3 +49,23 @@ def delete_task(user_id, task_id):
         return {"error": "User or task not found"}, 404
 
     return {"message": "Task deleted successfully"}, 200
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime, timedelta
+import pytz
+
+scheduler = BackgroundScheduler()
+scheduler.start()
+
+def send_reminder(task_name, user_id, reminder_time):
+    print("Reminder for task:", task_name, flush=True)
+
+def schedule_task_reminders(tasks, user_id):
+    utc_zone = pytz.utc
+    for task in tasks:
+        start_time_utc = datetime.strptime(task['startDateTime'], '%Y-%m-%dT%H:%M').replace(tzinfo=utc_zone)
+        reminder_times = [start_time_utc - timedelta(minutes=15, hours=1), start_time_utc - timedelta(minutes=10, hours=1)]
+        
+        for reminder_time in reminder_times:
+            if reminder_time > datetime.now(pytz.utc):
+                scheduler.add_job(send_reminder, 'date', run_date=reminder_time, args=[task['name'], user_id, reminder_time.strftime('%Y-%m-%d %H:%M:%S')])
